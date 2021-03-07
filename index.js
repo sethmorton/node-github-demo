c13019ac7533826a33c0fbaa3dded78578298885
Seth Morton <sethmorton@Seths-MacBook-Air.local>
2021-03-06 17:04:51 -0800

    node app with test cases

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+    return res.send("Hello World")
+})
+app.listen(process.env.PORT || 8888, () => {
+    console.log("Server is running")
+})
+module.exports = app;
